d6b807bcae0565d8ac47fda13bda803e0074a963
munisisaza <munis.isaza@stanga.net>
2021-11-16 22:42:33 +0400

    New changes

--- mysite/settings.py
+++ mysite/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -74,10 +75,9 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+    "default": dj_database_url.parse("postgres://dbumlehujjjzwx:c1f2b55781752614200db6901b549c934fddee"
+                                     "68a7e72bbee9a7cd094fa6b23c@ec2-54-220-243-77.eu-west-1.compute.a"
+                                     "mazonaws.com:5432/d9rdvrkbtskflk")
 }
 
 
